aed70f523ae9f50020b35114251fafb2acb7d7ce
Vukovic Nikola <admin@nikolav.rs>
2025-08-30 19:28:31 +0200

    Sat Aug 30 17:28:31 UTC 2025

--- src/config/firebase_admin/__init__.py
+++ src/config/firebase_admin/__init__.py
@@ -5,9 +5,17 @@ from firebase_admin import credentials
 from src.config import Config
 
 
-# service account key file
-cert = credentials.Certificate(f'./{Config.CERTIFICATE_FIREBASEADMINSDK}')
+initialized = False
+cert        = None
 
-# Initialize the Firebase app
-firebase_admin.initialize_app(cert)
+if not initialized:
+
+  # service account key file
+  cert = credentials.Certificate(f'./{Config.CERTIFICATE_FIREBASEADMINSDK}')
+
+  # Initialize the Firebase app
+  firebase_admin.initialize_app(cert)
+
+  initialized = True
+
 
