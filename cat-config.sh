e66a51b1e9032bb7040857d09aba55a6e71cacd4
Vukovic Nikola <admin@nikolav.rs>
2025-08-31 17:59:32 +0200

    Sun, Aug 31, 2025  3:59:31 PM

--- cat-config.sh
+++ cat-config.sh
@@ -9,6 +9,6 @@ cat ./deploy-vars.sh
 echo '@@ ./src/config/vars.py'
 cat ./src/config/vars.py
 
-echo '@@ ./ngapp---iec2cy5qtf---dev-firebase-adminsdk-ynt8w-78ca6332f5.json'
-cat ./ngapp---iec2cy5qtf---dev-firebase-adminsdk-ynt8w-78ca6332f5.json
+echo '@@ ./ngapp---iec2cy5qtf---dev-firebase-adminsdk-ynt8w-43abed87ff.json'
+cat ./ngapp---iec2cy5qtf---dev-firebase-adminsdk-ynt8w-43abed87ff.json
 
--- scp-config.sh
+++ scp-config.sh
@@ -7,5 +7,5 @@ scp ./.env root@$API_HOST:$APP_PATH
 # scp ./db/.env root@$API_HOST:$APP_PATH/db
 scp ./deploy-vars.sh root@$API_HOST:$APP_PATH
 scp ./src/config/vars.py root@$API_HOST:$APP_PATH/src/config/vars.py
-scp ./ngapp---iec2cy5qtf---dev-firebase-adminsdk-ynt8w-78ca6332f5.json root@$API_HOST:$APP_PATH
+scp ./ngapp---iec2cy5qtf---dev-firebase-adminsdk-ynt8w-43abed87ff.json root@$API_HOST:$APP_PATH
 # scp ./redis/redis.conf root@$API_HOST:$APP_PATH/redis
